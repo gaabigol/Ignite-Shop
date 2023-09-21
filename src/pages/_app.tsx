5135cf519fdc6d6b2de95bd0a8db966440afacfc
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-09-21 21:22:53 +0100

    feat: created global styles and stitches configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
+import Head from "next/head";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  style: ["normal"],
+  weight: ["400", "700"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <div>
+      <Head>
+        <title>Ignite-Shop</title>
+        <meta name="description" content="Ignite-Shop" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      
+      </Head>
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
